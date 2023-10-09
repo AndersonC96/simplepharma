d9e209fd0c373863e2f4e17a7fc913f6ce81c2ee
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-09 18:23:43 -0300

    Update structure.lua

--- structure.lua
+++ structure.lua
@@ -1,23 +1,23 @@
 project/
 |--admin/
-|	|--abrirchamadoAdmin.php | Página para abrir chamado • Feito
-|	|--adminHome.php | Página inicial do administrador • Feito
-|	|--chamadosAbertos.php | Página para ver chamados abertos • Feito
-|	|--chamamadosConcluidos.php | Página para ver chamados concluídos • 
-|	|--deletarchamdoAdmin.php | Página para deletar chamados • 
-|	|--processainsereChamado.php | Página para inserir chamados • 
-|	|--verchamadosAdmin.php | Página para ver chamados • 
-|	|--verTecnicos.php | Página para ver técnicos • 
-|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
-|	|--conexao.php | Página para conectar ao banco • 
-|	|--deleteOS.php | Página para deletar chamados • 
-|	|--deleteTec.php | Página para deletar técnicos • 
-|	|--deleteTec2.php | Página para deletar técnicos • 
-|	|--inseretecnicoRes.php | Página para inserir técnicos • 
-|	|--logout.php | Página para fazer logout • 
-|	|--processacadTecnic.php | Página para cadastrar técnicos • 
-|	|--removetecnicoRes.php | Página para remover técnicos • 
-|	|--ver1chamadoAdmin.php | Página para ver chamados • 
+|	|--abrirchamadoAdmin.php        | Página para abrir chamado             • Feito
+|	|--adminHome.php                | Página inicial do administrador       • Feito
+|	|--chamadosAbertos.php          | Página para ver chamados abertos      • Feito
+|	|--chamamadosConcluidos.php     | Página para ver chamados concluídos   • Feito
+|	|--conexao.php                  | Página para conectar ao banco         • Feito
+|	|--conexaodbAdmin.php           | Página para puxar dados do banco      • Feito
+|	|--deletarchamadoAdmin.php      | Página para deletar chamados          • Feito
+|	|--deleteOS.php                 | Página para deletar chamados          • Feito
+|	|--deleteTec.php                | Página para deletar técnicos          • Feito
+|	|--deleteTec2.php               | Página para deletar técnicos          • Feito -- Não sera usada
+|	|--inseretecnicoRes.php         | Página para inserir técnicos          • Feito
+|	|--logout.php                   | Página para fazer logout              • Feito
+|	|--processacadTecnic.php        | Página para cadastrar técnicos        • Feito
+|	|--processainsereChamado.php    | Página para inserir chamados          • Feito
+|	|--removetecnicoRes.php         | Página para remover técnicos          • Feito
+|	|--ver1chamadoAdmin.php         | Página para ver chamados              • Feito
+|	|--verchamadosAdmin.php         | Página para ver chamados              • Feito
+|	|--verTecnicos.php              | Página para ver técnicos              • Feito
 |--subAdmin/
 |	|--abrirchamadoSadmin.php | Página para abrir chamado • 
 |	|--chamadosAbertos.php | Página para ver chamados abertos • 
@@ -26,7 +26,7 @@ project/
 |	|--processainsereChamado.php | Página para inserir chamados • 
 |	|--subadminHome.php | Página inicial do subadministrador • 
 |	|--verchamadosSadmin.php | Página para ver chamados • 
-|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
+|	|--conexaodbAdmin.php | Página para puxar dados do banco • 
 |	|--conexao.php | Página para conectar ao banco • 
 |	|--logout.php | Página para fazer logout • 
 |	|--ver1chamadoAdmin.php | Página para ver chamados •
@@ -36,13 +36,13 @@ project/
 |	|--processaChamado.php | Página para processar chamados • 
 |	|--tecnicoHome.php | Página inicial do técnico • 
 |	|--verchamadosTec.php | Página para ver chamados • 
-|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
+|	|--conexaodbAdmin.php | Página para puxar dados do banco • 
 |	|--conexao.php | Página para conectar ao banco • 
 |	|--fazerchamadoTec.php | Página para fazer chamados • 
 |	|--logout.php | Página para fazer logout • 
 |	|--ver1chamadoTecnico.php | Página para ver chamados •
-|--autenticacaoUsuario.php | Página para autenticar usuário • Feito
-|--autenticar-usuario____.php | Página para autenticar usuário • Feito -- Não sera usada
+|--autenticarUsuario.php | Página para autenticar usuário • Feito
+|--autenticarusuario(naousar).php | Página para autenticar usuário • Feito -- Não sera usada
 |--coneexao.php | Página para conectar ao banco • Feito
 |--index.php | Página inicial • Feito
 |--logout.php | Página para fazer logout • Feito
