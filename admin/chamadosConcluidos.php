43749b0729aa07af1ee49946e21f331974a271dd
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-09 14:29:45 -0300

    Update chamadosConcluidos.php

--- admin/chamadosConcluidos.php
+++ admin/chamadosConcluidos.php
@@ -1,4 +1,5 @@
 <?php
+	error_reporting(0);
 	include("conexao.php");
 	$itens_por_pagina = 10;
 	$pagina = intval($_GET['pagina']);
@@ -20,7 +21,7 @@
 <?php
 	include("conexaodbAdmin.php");
 	$sql_code2 = "select * from chamados WHERE Status='Aberto'";
-	$execute2 = $mysqli->query($sql_code2) or die ($mysqli->error);
+	$execute2 = $mysqli->query($sql_code2) or die($mysqli->error);
 	$produto2 = $execute->fetch_assoc();
 	$num2 = $execute2->num_rows;
 ?>
@@ -30,8 +31,11 @@
 		<title>Chamados Concluídos</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" type="image/png" href="./img/favicon.png"/>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<link rel="icon" type="image/png" href="../img/favicon.png"/>
 	</head>
 	<body>
 		<nav class="navbar navbar-inverse">
@@ -42,7 +46,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#"><b style="Color: rgb(62 166 173)">Simple Pharma</b>Chamados</a>
+					<a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b> Chamados</a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav">
@@ -67,7 +71,7 @@
 						</li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>Sair</a></li>
+						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
 					</ul>
 				</div>
 			</div>
@@ -93,14 +97,14 @@
 						<td><?php echo $produto['local'];?></td>
 						<td><?php echo $produto['tecnico']; ?></td>
 						<td><?php echo $produto['datahora']; ?></td>
-						<?php if($produto['status']=="Aberto"){?>
+						<?php if ($produto['status']=="Aberto"){?>
 						<td style="background-color:#F00;"> <?php echo $produto['status']; ?></td>
-						<?php } elseif ($produto['status']=="Feito") {?>
+						<?php }elseif ($produto['status']=="Feito") {?>
 						<td style="background-color:#0F0;"> <?php echo $produto['status']; ?></td>
 						<?php } ?>
 						<td><a class="btn btn-info btn-sm" href="ver1chamadoAdmin.php?chamado=<?php echo $produto['contador'];?>"data-toggle="tooltip" title="Detalhes"><span class="glyphicon glyphicon-share"></span>Ver</button></td>
 					</tr>
-					<?php } while($produto = $execute->fetch_assoc()); ?>
+					<?php }while($produto = $execute->fetch_assoc()); ?>
 				</tbody>
 			</table>
 			<nav>
@@ -114,7 +118,7 @@
 						for($i=0;$i<$num_paginas;$i++){
 							$estilo = "";
 							if($pagina == $i)
-							$estilo = "class=\"active\"";
+								$estilo = "class=\"active\"";
 					?>
 					<li <?php echo $estilo; ?> ><a href="chamadosConcluidos.php?pagina=<?php echo $i; ?>"><?php echo $i+1; ?></a></li>
 					<?php } ?>
@@ -127,7 +131,11 @@
 			</nav>
 			<?php } ?>
 		</div>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script>
 			$(document).ready(function(){
 				$('[data-toggle="tooltip"]').tooltip();
