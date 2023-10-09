3430f48e550c6ff1cb41f78856134bd7818be7d8
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-09 09:19:39 -0300

    Update adminHome.php

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -18,10 +18,7 @@
         <title>Área Administrativa | Home</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
         <nav class="navbar navbar-inverse">
@@ -39,7 +36,7 @@
                         <li class="active"><a href="Admin-Home.php">Home</a></li>
                         <li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Chamados<span class="caret"></span></a>
                             <ul class="dropdown-menu multi-level">
-                                <li><a href="Abrir-chamado-admin.php">Abrir Chamado</a></li>
+                                <li><a href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
                                 <li><a href="Deletar-chamado-admin.php">Deletar Chamado</a></li>
                                 <li><a href="Chamados_abertos.php">Chamados em Aberto <span class="badge badge-danger"><?php echo $num;?></span></a></li>
                                 <li><a href="Chamados_concluidos.php">Chamados Concluídos</a></li>
@@ -67,4 +64,5 @@
             <p>Algumas funcionalidades podem não estar ativas ainda</p>
         </div>
     </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
