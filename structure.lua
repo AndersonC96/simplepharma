caddbf93b0cd00d3a707be61a168d23416914878
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-09 10:59:53 -0300

    Create structure.lua

---
+++ structure.lua
@@ -0,0 +1,47 @@
+project/
+|--admin/
+|	|--abrirchamadoAdmin.php | Página para abrir chamado • Feito
+|	|--adminHome.php | Página inicial do administrador • Feito
+|	|--chamadosAbertos.php | Página para ver chamados abertos • Feito
+|	|--chamamadosConcluidos.php | Página para ver chamados concluídos • 
+|	|--deletarchamdoAdmin.php | Página para deletar chamados • 
+|	|--processainsereChamado.php | Página para inserir chamados • 
+|	|--verchamadosAdmin.php | Página para ver chamados • 
+|	|--verTecnicoc.php | Página para ver técnicos • 
+|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
+|	|--conexao.php | Página para conectar ao banco • 
+|	|--deleteOS.php | Página para deletar chamados • 
+|	|--deleteTec.php | Página para deletar técnicos • 
+|	|--deleteTec2.php | Página para deletar técnicos • 
+|	|--inseretecnicoRes.php | Página para inserir técnicos • 
+|	|--logout.php | Página para fazer logout • 
+|	|--processacadTecnic.php | Página para cadastrar técnicos • 
+|	|--ver1chamadoAdmin.php | Página para ver chamados • 
+|--subAdmin/
+|	|--abrirchamadoSadmin.php | Página para abrir chamado • 
+|	|--chamadosAbertos.php | Página para ver chamados abertos • 
+|	|--chamadosConcluidos.php | Página para ver chamados concluídos • 
+|	|--deletarchamadoSadmin.php | Página para deletar chamados • 
+|	|--processainsereChamado.php | Página para inserir chamados • 
+|	|--subadminHome.php | Página inicial do subadministrador • 
+|	|--verchamadosSadmin.php | Página para ver chamados • 
+|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
+|	|--conexao.php | Página para conectar ao banco • 
+|	|--logout.php | Página para fazer logout • 
+|	|--ver1chamadoAdmin.php | Página para ver chamados •
+|--tecnico/
+|	|--chamadosabertosTec.php | Página para ver chamados abertos • 
+|	|--chamadosconcluidosTec.php | Página para ver chamados concluídos • 
+|	|--processaChamado.php | Página para processar chamados • 
+|	|--tecnicoHome.php | Página inicial do técnico • 
+|	|--verchamadosTec.php | Página para ver chamados • 
+|	|--conectapuxadadosAdmin.php | Página para puxar dados do banco • 
+|	|--conexao.php | Página para conectar ao banco • 
+|	|--fazerchamadoTec.php | Página para fazer chamados • 
+|	|--logout.php | Página para fazer logout • 
+|	|--ver1chamadoTecnico.php | Página para ver chamados •
+|--autenticacaoUsuario.php | Página para autenticar usuário • Feito
+|--autenticar-usuario____.php | Página para autenticar usuário • Feito -- Não sera usada
+|--coneexao.php | Página para conectar ao banco • Feito
+|--index.php | Página inicial • Feito
+|--logout.php | Página para fazer logout • 
