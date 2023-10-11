597325d1c8516c46bea0b3182c7428f14d2594cb
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-11 16:06:11 -0300

    Update deleteOS.php

--- admin/deleteOS.php
+++ admin/deleteOS.php
@@ -38,8 +38,8 @@
       </div>
     </div>
     <?php
-      //$link = mysqli_connect("localhost", "root", "", "simplepharma");
-      $link = mysqli_connect($hostname_conexao, $username_conexao, $password_conexao, $database_conexao);
+      $link = mysqli_connect("localhost", "root", "", "simplepharma");
+      //$link = mysqli_connect($hostname_conexao, $username_conexao, $password_conexao, $database_conexao);
       $numero_os = $_POST['oser'];
       if($link === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
