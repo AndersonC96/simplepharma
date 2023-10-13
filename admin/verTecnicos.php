e615f1114a7afdba309f4ef6fe23d7fccf69832b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-13 14:38:43 -0300

    Update verTecnicos.php

--- admin/verTecnicos.php
+++ admin/verTecnicos.php
@@ -20,110 +20,101 @@
 ?>
 <?php
 	include("conexaodbAdmin.php");
-	$sql_code2 = "select * from chamados WHERE Status='Aberto'";
+	$sql_code2 = "select * from chamados WHERE status='Aberto'";
 	$execute2 = $mysqli->query($sql_code2) or die($mysqli->error);
 	$produto2 = $execute2->fetch_assoc();
 	$num2 = $execute2->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Todos os técnicos</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<link rel="icon" type="image/png" href="../img/favicon.png"/>
-	</head>
-	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b> Chamados</a>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="adminHome.php">Home</a></li>
-						<li>
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Chamados<span class="caret"></span></a>
-							<ul class="dropdown-menu multi-level">
-								<li><a href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
-								<li><a href="deletarchamadoAdmin.php">Deletar Chamado</a></li>
-								<li><a href="chamadosAbertos.php">Chamados em Aberto <span class="badge badge-danger"><?php echo $num2;?></span></a></li>
-								<li><a href="chamadosConcluidos.php">Chamados Concluídos</a></li>
-								<li><a href="verchamadosAdmin.php">Listar Chamado</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Técnico<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="inseretecnicoRes.php">Inserir Técnico</a></li>
-								<li><a href="removetecnicoRes.php">Remover Técnico</a></li>
-								<li><a href="verTecnicos.php">Ver Técnicos</a></li>
-							</ul>
-						</li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<div class="container">
-			<h2>Lista de técnicos</h2>
-			<p>Técnicos registrados no Sistema</p>
-			<table class="table table-striped table table-bordered">
-				<?php if($num > 0){ ?>
-				<thead>
-					<tr>
-						<th>Nome Sistema</th>
-						<th>Nome Completo</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php do{ ?>
-					<tr>
-						<td><?php echo $produto['nome'];?></td>
-						<td><?php echo $produto['nomecompleto'];?></td>
-					</tr>
-					<?php }while($produto = $execute->fetch_assoc()); ?>
-				</tbody>
-			</table>
-			<nav>
-				<ul class="pagination">
-					<li>
-						<a href="verTecnicos.php?pagina=0" aria-label="Previous">
-							<span aria-hidden="true">&laquo;</span>
-						</a>
-					</li>
-					<?php
-						for($i=0;$i<$num_paginas;$i++){
-							$estilo = "";
-							if($pagina == $i)
-								$estilo = "class=\"active\"";
-					?>
-					<li <?php echo $estilo; ?> ><a href="verTecnicos.php?pagina=<?php echo $i; ?>"><?php echo $i+1; ?></a></li>
-					<?php } ?>
-					<li>
-						<a href="verTecnicos.php?pagina=<?php echo $num_paginas-1; ?>" aria-label="Next">
-							<span aria-hidden="true">&raquo;</span>
-						</a>
-					</li>
-				</ul>
-			</nav>
-			<?php } ?>
-		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	</body>
+    <head>
+        <title><?php echo $_SESSION['sess_usersisname']; ?> | Todos os técnicos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link rel="icon" type="image/png" href="../img/favicon.png">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#"><b style="color: #35a8b1">Simple Pharma</b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active"><a class="nav-link" href="adminHome.php">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
+                            <ul class="dropdown-menu multi-level">
+                                <li><a class="dropdown-item" href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
+                                <li><a class="dropdown-item" href="deletarchamadoAdmin.php">Deletar Chamado</a></li>
+                                <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num2; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosConcluidos.php">Chamados Concluídos</a></li>
+                                <li><a class="dropdown-item" href="verchamadosAdmin.php">Listar Chamado</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="tecnicoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Técnico</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="inseretecnicoRes.php">Inserir Técnico</a></li>
+                                <li><a class="dropdown-item" href="removetecnicoRes.php">Remover Técnico</a></li>
+                                <li><a class="dropdown-item" href="verTecnicos.php">Ver Técnicos</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
+        <div class="container">
+            <h2>Lista de técnicos</h2>
+            <table class="table table-striped table table-bordered">
+                <?php if($num > 0){ ?>
+                <thead>
+                    <tr>
+                        <th class="text-center">Nome Sistema</th>
+                        <th class="text-center">Nome Completo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php do{ ?>
+                    <tr>
+                        <td class="text-center"><?php echo $produto['nome'];?></td>
+                        <td class="text-center"><?php echo $produto['nomecompleto'];?></td>
+                    </tr>
+                    <?php }while($produto = $execute->fetch_assoc()); ?>
+                </tbody>
+            </table>
+            <nav>
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="verTecnicos.php?pagina=0" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <?php
+                        for($i=0;$i<$num_paginas;$i++){
+                            $estilo = "";
+                            if($pagina == $i)
+                                $estilo = "class=\"page-item active\"";
+                    ?>
+                    <li <?php echo $estilo; ?> ><a class="page-link" href="verTecnicos.php?pagina=<?php echo $i; ?>"><?php echo $i+1; ?></a></li>
+                    <?php } ?>
+                    <li class="page-item">
+                        <a class="page-link" href="verTecnicos.php?pagina=<?php echo $num_paginas-1; ?>" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <?php } ?>
+        </div>
+    </body>
 </html>
