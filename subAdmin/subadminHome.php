0b4b3c37db91bf93d2f50c291b5b4a4c3d002cbe
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-13 15:05:56 -0300

    Update subadminHome.php

--- subAdmin/subadminHome.php
+++ subAdmin/subadminHome.php
@@ -1,62 +1,59 @@
 <?php
     session_start();
     $role = $_SESSION['sess_userrole'];
-    if(!isset($_SESSION['sess_username']) || $role!="subadmin"){
-		header('Location: ../index.php?err=2');
+    if(!isset($_SESSION['sess_username']) || $role != "subadmin"){
+        header('Location: ../index.php?err=2');
     }
 ?>
 <?php
-	include("conexaodbAdmin.php");
-	$sql_code = "select * from chamados WHERE Status='Aberto'";
-	$execute = $mysqli->query($sql_code) or die($mysqli->error);
-	$produto = $execute->fetch_assoc();
-	$num = $execute->num_rows;
+    include("conexaodbAdmin.php");
+    $sql_code = "select * from chamados WHERE status='Aberto'";
+    $execute = $mysqli->query($sql_code) or die($mysqli->error);
+    $produto = $execute->fetch_assoc();
+    $num = $execute->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title><?php echo $_SESSION['sess_usersisname']; ?> | Área Administrativa | Home</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<link rel="icon" type="image/png" href="../img/favicon.png"/>
-	</head>
-	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b> Chamados</a>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="subadminHome.php">Home</a></li>
-						<li>
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Chamados<span class="caret"></span></a>
-							<ul class="dropdown-menu multi-level">
-								<li><a href="abrirchamadoSadmin.php">Abrir Chamado</a></li>
-								<li><a href="deletarchamadoSadmin.php">Deletar Chamado</a></li>
-								<li><a href="chamadosAbertos.php">Chamados em Aberto <span class="badge badge-danger"><?php echo $num;?></span></a></li>
-								<li><a href="chamadosConcluidos.php">Chamados Concluídos</a></li>
-								<li><a href="verchamadosSadmin.php">Listar Chamado</a></li>
-							</ul>
-						</li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<div class="container">
-			<h1>Bem-vindo, <b style="color: rgb(83 168 177)"><?php echo $_SESSION['sess_usersisname']; ?></b></h2>
-		</div>
-	</body>
+    <head>
+        <title><?php echo $_SESSION['sess_usersisname']; ?> | Área Administrativa | Home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link rel="icon" type="image/png" href="../img/favicon.png">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#"><b style="color: #53A8B1">Simple Pharma</b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active"><a class="nav-link" href="subadminHome.php">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
+                            <ul class="dropdown-menu multi-level">
+                                <li><a class="dropdown-item" href="abrirchamadoSadmin.php">Abrir Chamado</a></li>
+                                <li><a class="dropdown-item" href="deletarchamadoSadmin.php">Deletar Chamado</a></li>
+                                <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosConcluidos.php">Chamados Concluídos</a></li>
+                                <li><a class="dropdown-item" href="verchamadosSadmin.php">Listar Chamado</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
+        <div class="container">
+            <h1>Bem-vindo, <b style="color: #53A8B1"><?php echo $_SESSION['sess_usersisname']; ?></b></h1>
+        </div>
+    </body>
 </html>
