39ca03ff1abb95d8baa10e6392e668e69b5f97e3
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-13 12:06:42 -0300

    Update deletarchamadoAdmin.php

--- admin/deletarchamadoAdmin.php
+++ admin/deletarchamadoAdmin.php
@@ -1,78 +1,76 @@
 <?php
-	error_reporting(0);
+    error_reporting(0);
     session_start();
     $role = $_SESSION['sess_userrole'];
-    if(!isset($_SESSION['sess_username']) || $role!="admin"){
-		header('Location: ../index.php?err=2');
+    if(!isset($_SESSION['sess_username']) || $role != "admin"){
+        header('Location: ../index.php?err=2');
     }
 ?>
 <?php
-	include("conexaodbAdmin.php");
-	$sql_code = "select * from chamados WHERE Status='Aberto'";
-	$execute = $mysqli->query($sql_code) or die($mysqli->error);
-	$produto = $execute->fetch_assoc();
-	$num = $execute->num_rows;
+    include("conexaodbAdmin.php");
+    $sql_code = "select * from chamados WHERE status='Aberto'";
+    $execute = $mysqli->query($sql_code) or die($mysqli->error);
+    $produto = $execute->fetch_assoc();
+    $num = $execute->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Deletar Chamado</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<link rel="icon" type="image/png" href="../img/favicon.png"/>
-	</head>
-	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b> Chamados</a>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="adminHome.php">Home</a></li>
-						<li>
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Chamados<span class="caret"></span></a>
-							<ul class="dropdown-menu multi-level">
-								<li><a href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
-								<li><a href="#">Deletar Chamado</a></li>
-								<li><a href="chamadosAbertos.php">Chamados em Aberto <span class="badge badge-danger"><?php echo $num;?></span></a></li>
-								<li><a href="chamadosConcluidos.php">Chamados Concluídos</a></li>
-								<li><a href="verchamadosAdmin.php">Listar Chamado</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Técnico<span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="inseretecnicoRes.php">Inserir Técnico</a></li>
-								<li><a href="removetecnicoRes.php">Remover Técnico</a></li>
-								<li><a href="verTecnicos.php">Ver Técnicos</a></li>
-							</ul>
-						</li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<div class="container">
-			<h2>Preencha com o número da OS que deseja remover</h2>
-			<form method="POST" action="deleteOS.php">
-				<div class="form-group">
-					<!--<label for="local">Número da OS</label>-->
-					<input type="number" class="form-control" id="oser" placeholder="Número OS" name="oser" required />
-				</div>
-				<button type="submit" class="btn btn-default">Remover Ordem</button>
-			</form>
-		</div>
-	</body>
+    <head>
+        <title><?php echo $_SESSION['sess_usersisname']; ?> | Deletar Chamado</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="icon" type="image/png" href="../img/favicon.png"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active"><a class="nav-link" href="adminHome.php">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
+                            <ul class="dropdown-menu" aria-labelledby="chamadosDropdown">
+                                <li><a class="dropdown-item" href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
+                                <li><a class="dropdown-item" href="#">Deletar Chamado</a></li>
+                                <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosConcluidos.php">Chamados Concluídos</a></li>
+                                <li><a class="dropdown-item" href="verchamadosAdmin.php">Listar Chamado</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="tecnicoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Técnico</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="inseretecnicoRes.php">Inserir Técnico</a></li>
+                                <li><a class="dropdown-item" href="removetecnicoRes.php">Remover Técnico</a></li>
+                                <li><a class="dropdown-item" href="verTecnicos.php">Ver Técnicos</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
+        <div class="container">
+            <h2>Preencha com o número da OS que deseja remover</h2>
+            <form method="POST" action="deleteOS.php">
+                <div class="form-group">
+                    <input type="number" class="form-control" id="oser" placeholder="Número OS" name="oser" required />
+                </div>
+                <br>
+                <button type="submit" class="btn btn-danger">Remover Ordem</button>
+            </form>
+        </div>
+    </body>
 </html>
