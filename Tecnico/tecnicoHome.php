18f03661759cd2839dab8351756bb1f13d315898
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-13 17:36:37 -0300

    Update tecnicoHome.php

--- Tecnico/tecnicoHome.php
+++ Tecnico/tecnicoHome.php
@@ -1,61 +1,56 @@
 <?php
     session_start();
     $role = $_SESSION['sess_userrole'];
-    if(!isset($_SESSION['sess_username']) ||$role!="tecnico"){
-      header('Location: ../index.php?err=2');
+    if(!isset($_SESSION['sess_username']) || $role != "tecnico"){
+        header('Location: ../index.php?err=2');
     }
 ?>
 <?php
-  include("conexaodbAdmin.php");
-  $tecnico = $_SESSION['sess_username'];
-  $sql_code = "select * from chamados WHERE status='Aberto' AND tecnico='$tecnico'";
-  $execute = $mysqli->query($sql_code) or die ($mysqli->error);
-  $produto = $execute->fetch_assoc();
-  $num = $execute->num_rows;
+    include("conexaodbAdmin.php");
+    $tecnico = $_SESSION['sess_username'];
+    $sql_code = "select * from chamados WHERE status='Aberto' AND tecnico='$tecnico'";
+    $execute = $mysqli->query($sql_code) or die($mysqli->error);
+    $produto = $execute->fetch_assoc();
+    $num = $execute->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Área Técnico</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <link rel="icon" type="image/png" href="../img/favicon.png"/>
-  </head>
-  <body>
-    <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b> Chamados</a>
+    <head>
+        <title><?php echo $_SESSION['sess_usersisname']; ?> | Área Técnico | Home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link rel="icon" type="image/png" href="../img/favicon.png"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b></a>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item active"><a class="nav-link" href="tecnicoHome.php">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
+                            <ul class="dropdown-menu" aria-labelledby="chamadosDropdown">
+                                <li><a class="dropdown-item" href="chamadosabertosTec.php">Chamados em Aberto<span class="badge badge-danger"><?php echo $num; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosconcluidosTec.php">Chamados Concluídos</a></li>
+                                <li><a class="dropdown-item" href="verchamadosTec.php">Listar Chamado</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
+        <div class="container">
+            <h1>Bem-vindo, <b style="color: rgb(83 168 177)"><?php echo $_SESSION['sess_usersisname']; ?></b></h1>
         </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="tecnicoHome.php">Home</a></li>
-            <li>
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Chamados<span class="caret"></span></a>
-              <ul class="dropdown-menu multi-level">
-                <li><a href="chamadosabertosTec.php">Chamados em Aberto <span class="badge badge-danger"><?php echo $num;?></span></a></li>
-                <li><a href="chamadosconcluidosTec.php">Chamados Concluídos</a></li>
-                <li><a href="verchamadosTec.php">Listar Chamado</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container">
-      <h4>Bem vindo Técnico <?php echo $_SESSION['sess_usersisname'];?></h4>
-    </div>
-  </body>
+    </body>
 </html>
