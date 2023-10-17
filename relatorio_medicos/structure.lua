ab2958c761873ac9725ba22af703900f4724a9af
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-17 11:21:17 -0300

    Create structure.lua

---
+++ relatorio_medicos/structure.lua
@@ -0,0 +1,22 @@
+project/
+|---index.php | Página inicial
+|---login/
+│   |---login.php | Página de login
+│   |---processa_login.php | Script de processamento de login
+|---upload/
+│   |---upload.php | Página de upload
+│   |---processa_upload.php | Script de processamento de upload
+│   |---uploads/
+|---dados/
+│   |---dados.php | Página de dados
+|---includes/
+│   |---db_connection.php | Script de conexão com o banco de dados
+│   |---functions.php
+|---assets/
+│   |---css/ | Arquivos CSS
+│   |---js/ | Arquivos JavaScript
+|---config/
+│   |---config.php | Arquivo de configuração
+|---templates/
+│   |---header.php | Cabeçalho comum
+│   |---footer.php | Rodapé comum
