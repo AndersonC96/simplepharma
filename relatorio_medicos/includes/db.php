8cacbe111439dc7fcfdd2ba8f58ef3fc5acfaa8b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-10-17 11:39:04 -0300

    Create db.php

---
+++ relatorio_medicos/includes/db.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "medicos";
+    $conn = new mysqli($host, $username, $password, $database);
+    if($conn->connect_error){
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+?>
