042a2f7ea71a297ab21c975cba3a80cebae47c21
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-02-27 10:33:09 -0300

    Update chamadosAbertos.php

--- subAdmin/chamadosAbertos.php
+++ subAdmin/chamadosAbertos.php
@@ -1,22 +1,16 @@
 <?php
+    session_start();
     error_reporting(0);
     include("conexao.php");
     $itens_por_pagina = 10;
-    $pagina = intval($_GET['pagina']);
+    $pagina = isset($_GET['pagina']) ? intval($_GET['pagina']) : 0;
     $item = $pagina * $itens_por_pagina;
-    $sql_code = "select contador, local, tecnico, datahora, status, servico from chamados WHERE status='Aberto' ORDER BY contador DESC LIMIT $item, $itens_por_pagina";
+    $sql_code = "SELECT contador, local, tecnico, datahora, status, servico FROM chamados WHERE status='Aberto' AND usuario='{$_SESSION['sess_usersisname']}' ORDER BY contador DESC LIMIT $item, $itens_por_pagina";
     $execute = $conn->query($sql_code) or die($conn->error);
     $produto = $execute->fetch_assoc();
     $num = $execute->num_rows;
-    $num_total = $conn->query("select contador, local, tecnico, datahora, status, servico from chamados WHERE status='Aberto'")->num_rows;
+    $num_total = $conn->query("SELECT contador, local, tecnico, datahora, status, servico FROM chamados WHERE status='Aberto' AND usuario='{$_SESSION['sess_usersisname']}'")->num_rows;
     $num_paginas = ceil($num_total / $itens_por_pagina);
-?>
-<?php
-    session_start();
-    $role = $_SESSION['sess_userrole'];
-    if(!isset($_SESSION['sess_username']) || $role != "subadmin"){
-        header('Location: ../index.php?err=2');
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,8 +19,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="../img/favicon.png" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
