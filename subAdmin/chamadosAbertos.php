230acc7c8e267910fd5a9a4e5d4f5d6c42f6c804
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-22 17:26:54 -0300

    Update chamadosAbertos.php

--- subAdmin/chamadosAbertos.php
+++ subAdmin/chamadosAbertos.php
@@ -21,22 +21,24 @@
         <link rel="icon" type="image/png" href="../img/favicon.png" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link href="../CSS/nav.css" rel="stylesheet">
+        <link href="../CSS/body_chamado.css" rel="stylesheet">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#"><b style="color: rgb(83 168 177)">Simple Pharma</b></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar">
+        <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary px-0 py-3">
+            <div class="container-xl">
+                <a class="navbar-brand" href="#">
+                    <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" class="h-12" alt="...">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="subadminHome.php">Home</a>
-                        </li>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-lg-auto">
+                        <a class="nav-item nav-link active" href="subadminHome.php" aria-current="page">Home</a>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
-                            <ul class="dropdown-menu multi-level">
+                            <ul class="dropdown-menu" aria-labelledby="chamadosDropdown">
                                 <li><a class="dropdown-item" href="abrirchamadoSadmin.php">Abrir Chamado</a></li>
                                 <li><a class="dropdown-item" href="deletarchamadoSadmin.php">Deletar Chamado</a></li>
                                 <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
@@ -44,12 +46,13 @@
                                 <li><a class="dropdown-item" href="verchamadosSadmin.php">Listar Chamado</a></li>
                             </ul>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a>
-                        </li>
-                    </ul>
+                    </div>
+                    <div class="navbar-nav ms-lg-4">
+                        <a class="nav-item nav-link" href="#"><?php echo $_SESSION['sess_usersisname']; ?></a>
+                    </div>
+                    <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
+                        <a href="logout.php" class="btn btn-sm btn-secondary w-full w-lg-auto">Sair</a>
+                    </div>
                 </div>
             </div>
         </nav>
