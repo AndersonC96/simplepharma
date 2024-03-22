0a7f46b101131134fccbab28c0e0f6521711de22
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-22 17:13:42 -0300

    Update verTecnicos.php

--- admin/verTecnicos.php
+++ admin/verTecnicos.php
@@ -34,41 +34,86 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <link rel="icon" type="image/png" href="../img/favicon.png">
+        <link href="../CSS/nav.css" rel="stylesheet">
+        <link href="../CSS/body_chamado.css" rel="stylesheet">
+        <style>
+            .table thead th{
+                background-color: #008d93;
+                color: #ffffff;
+            }
+            .pagination li a{
+                color: #008d93;
+            }
+            .pagination li.active a, .pagination li a:hover{
+                color: #ffffff;
+                background-color: #008d93;
+                border-color: #008d93;
+            }
+            .pagination{
+                justify-content: center;
+            }
+            .pagination .page-link{
+                color: #008d93;
+                background-color: #ffffff;
+                border-color: #008d93;
+            }
+            .pagination .page-item.active .page-link{
+                color: #ffffff;
+                background-color: #008d93;
+                border-color: #008d93;
+            }
+            .pagination .page-link:hover{
+                color: #ffffff;
+                background-color: #007a7a;
+                border-color: #007a7a;
+            }
+        </style>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#"><b style="color: #35a8b1">Simple Pharma</b></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary px-0 py-3">
+            <div class="container-xl">
+                <a class="navbar-brand" href="#">
+                    <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" class="h-12" alt="...">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active"><a class="nav-link" href="adminHome.php">Home</a></li>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-lg-auto">
+                        <a class="nav-item nav-link active" href="adminHome.php" aria-current="page">Home</a>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
-                            <ul class="dropdown-menu multi-level">
+                            <ul class="dropdown-menu" aria-labelledby="chamadosDropdown">
                                 <li><a class="dropdown-item" href="abrirchamadoAdmin.php">Abrir Chamado</a></li>
                                 <li><a class="dropdown-item" href="deletarchamadoAdmin.php">Deletar Chamado</a></li>
-                                <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num2; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosAbertos.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
                                 <li><a class="dropdown-item" href="chamadosConcluidos.php">Chamados Concluídos</a></li>
                                 <li><a class="dropdown-item" href="verchamadosAdmin.php">Listar Chamado</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="tecnicoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Técnico</a>
-                            <ul class="dropdown-menu">
+                            <ul class="dropdown-menu" aria-labelledby="tecnicoDropdown">
                                 <li><a class="dropdown-item" href="inseretecnicoRes.php">Inserir Técnico</a></li>
                                 <li><a class="dropdown-item" href="removetecnicoRes.php">Remover Técnico</a></li>
                                 <li><a class="dropdown-item" href="verTecnicos.php">Ver Técnicos</a></li>
                             </ul>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="tecnicoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Usuários</a>
+                            <ul class="dropdown-menu" aria-labelledby="tecnicoDropdown">
+                                <li><a class="dropdown-item" href="insereUsuario.php">Inserir Usuário</a></li>
+                                <li><a class="dropdown-item" href="removeUsuario.php">Remover Usuário</a></li>
+                                <li><a class="dropdown-item" href="verUsuarios.php">Ver Usuários</a></li>
+                            </ul>
                         </li>
-                    </ul>
+                    </div>
+                    <div class="navbar-nav ms-lg-4">
+                        <a class="nav-item nav-link" href="#"><?php echo $_SESSION['sess_usersisname']; ?></a>
+                    </div>
+                    <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
+                        <a href="logout.php" class="btn btn-sm btn-secondary w-full w-lg-auto">Sair</a>
+                    </div>
                 </div>
             </div>
         </nav>
