de4b4cff3895e138969cf7a45328c0fe491678a8
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-22 17:21:33 -0300

    Update abrirchamadoAdmin.php

--- admin/abrirchamadoAdmin.php
+++ admin/abrirchamadoAdmin.php
@@ -29,7 +29,7 @@
         <link href="../CSS/body_chamado.css" rel="stylesheet">
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary px-0 py-3">
+        <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary px-0 py-3">
             <div class="container-xl">
                 <a class="navbar-brand" href="#">
                     <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" class="h-12" alt="...">
