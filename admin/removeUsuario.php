70bcad531dc5e07f984ee11354a69cff93f0a0e6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-23 18:26:19 -0300

    Update removeUsuario.php

--- admin/removeUsuario.php
+++ admin/removeUsuario.php
@@ -15,7 +15,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title><?php echo $_SESSION['sess_usersisname']; ?> | Remover Técnico</title>
+        <title><?php echo $_SESSION['sess_usersisname']; ?> | Remover Usuário</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
