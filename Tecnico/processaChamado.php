0cbac289e77c46c40600ea2686b347a5137d8ee7
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-27 12:16:04 -0300

    Update processaChamado.php

--- Tecnico/processaChamado.php
+++ Tecnico/processaChamado.php
@@ -17,7 +17,7 @@
           <div class="modal-header">
             <h4 class="modal-title">Atenção</h4>
           </div>
-          <div class="modal-body">Chamado realizado com sucesso !</div>
+          <div class="modal-body">Chamado realizado com sucesso!</div>
           <div class="modal-footer">
             <a class="btn btn-success" href="tecnicoHome.php">Entendido</a>
           </div>
@@ -27,23 +27,26 @@
     <?php
       session_start();
       $role = $_SESSION['sess_userrole'];
-      if(!isset($_SESSION['sess_username']) || $role!="tecnico"){
+      if (!isset($_SESSION['sess_username']) || $role != "tecnico") {
         header('Location: ../index.php?err=2');
       }
-?>
-<?php
-  include_once("conexao.php");
-  $servicoexecutado = $_POST['servicoexe'];
-  $datainiciofez = $_POST['datahoraaber'];
-  $numeroos = $_POST['var'];
-  $datafimfez = $_POST['datahorafim'];
-  $status = "Feito";
-  $query = "UPDATE chamados SET serviexecu = '".$servicoexecutado."', datahoraaber = '".$datainiciofez."', datahorafim = '".$datafimfez."',status = '".$status."' WHERE (contador = ".$numeroos.")";
-  $resultado_usuario = mysqli_query($conn, $query);
-  if($conn->query($query) === TRUE){
-    echo '<script type="text/javascript"> $("#myModal2").modal("show")</script>';
-  }else{
-    echo "Error updating record: " . $conn->error;
-  }
-  $conn->close();
-?>
+    ?>
+    <?php
+      include_once("conexao.php");
+      $servicoexecutado = $_POST['servicoexe'];
+      $datainiciofez = $_POST['datahoraaber'];
+      $numeroos = $_POST['var'];
+      $datafimfez = $_POST['datahorafim'];
+      $status = $_POST['status'];
+      $comentarios = $_POST['comentarios'];
+      $query = "UPDATE chamados SET serviexecu = '$servicoexecutado', datahoraaber = '$datainiciofez', datahorafim = '$datafimfez', status = '$status', comentarios = '$comentarios' WHERE contador = '$numeroos'";
+      $resultado_usuario = mysqli_query($conn, $query);
+      if($conn->query($query) === TRUE){
+        echo '<script type="text/javascript"> $("#myModal2").modal("show");</script>';
+      }else{
+        echo "Error updating record: " . $conn->error;
+      }
+      $conn->close();
+    ?>
+  </body>
+</html>
