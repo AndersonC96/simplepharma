be52980408a2662bcfd4ad9eecfc6255a86f1c08
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-07-17 16:41:33 -0300

    Update removeUsuario.php

--- admin/removeUsuario.php
+++ admin/removeUsuario.php
@@ -49,7 +49,7 @@
                                 <li><a class="dropdown-item" href="deletarchamadoAdmin.php"><i class="fas fa-trash-alt"></i> Deletar Chamado</a></li>
                                 <li><a class="dropdown-item" href="chamadosAbertos.php"><i class="fas fa-exclamation-circle"></i> Chamados em Aberto <span class="badge bg-danger"><?php echo $num2; ?></span></a></li>
                                 <li><a class="dropdown-item" href="chamadosConcluidos.php"><i class="fas fa-check-circle"></i> Chamados Concluídos</a></li>
-                                <li><a class="dropdown-item" href="verchamadosAdmin.php"><i class="fas fa-list"></i> Listar Chamado</a></li>
+                                <li><a class="dropdown-item" href="verchamadosAdmin.php"><i class="fas fa-list"></i> Listar Chamados</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
