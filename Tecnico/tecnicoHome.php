1485f2af53994477f2fa774c76e87c57dd786cca
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-07-22 15:45:01 -0300

    Update tecnicoHome.php

--- Tecnico/tecnicoHome.php
+++ Tecnico/tecnicoHome.php
@@ -3,24 +3,25 @@
     $role = $_SESSION['sess_userrole'];
     if(!isset($_SESSION['sess_username']) || $role != "tecnico"){
         header('Location: ../index.php?err=2');
+        exit();
     }
 ?>
 <?php
     include("conexaodbAdmin.php");
     $tecnico = $_SESSION['sess_username'];
-    $sql_code = "select * from chamados WHERE status='Aberto' AND tecnico='$tecnico'";
+    $sql_code = "SELECT * FROM chamados WHERE status='Aberto' AND tecnico='$tecnico'";
     $execute = $mysqli->query($sql_code) or die($mysqli->error);
     $produto = $execute->fetch_assoc();
     $num = $execute->num_rows;
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title><?php echo $_SESSION['sess_usersisname']; ?> | Área Técnico | Home</title>
+        <title><?php echo htmlspecialchars($_SESSION['sess_usersisname']); ?> | Área Técnico | Home</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
         <link rel="icon" type="image/png" href="../img/favicon.png"/>
         <link href="../CSS/nav.css" rel="stylesheet">
     </head>
@@ -35,30 +36,41 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav mx-lg-auto">
-                        <a class="nav-item nav-link active" href="tecnicoHome.php" aria-current="page">Home</a>
+                        <a class="nav-item nav-link active" href="tecnicoHome.php" aria-current="page">
+                            <i class="bi bi-house-door"></i> Home
+                        </a>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chamados</a>
+                            <a class="nav-link dropdown-toggle" href="#" id="chamadosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-envelope"></i> Chamados
+                            </a>
                             <ul class="dropdown-menu" aria-labelledby="chamadosDropdown">
-                                <li><a class="dropdown-item" href="chamadosabertosTec.php">Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
-                                <li><a class="dropdown-item" href="chamadosconcluidosTec.php">Chamados Concluídos</a></li>
-                                <li><a class="dropdown-item" href="verchamadosTec.php">Listar Chamado</a></li>
+                                <li><a class="dropdown-item" href="chamadosabertosTec.php"><i class="bi bi-exclamation-circle"></i> Chamados em Aberto <span class="badge bg-danger"><?php echo $num; ?></span></a></li>
+                                <li><a class="dropdown-item" href="chamadosconcluidosTec.php"><i class="bi bi-check-circle"></i> Chamados Concluídos</a></li>
+                                <li><a class="dropdown-item" href="verchamadosTec.php"><i class="bi bi-list"></i> Listar Chamados</a></li>
                             </ul>
                         </li>
                     </div>
                     <div class="navbar-nav ms-lg-4">
-                        <a class="nav-item nav-link" href="#"><?php echo $_SESSION['sess_usersisname']; ?></a>
+                        <a class="nav-item nav-link" href="#"><i class="bi bi-person"></i> <?php echo htmlspecialchars($_SESSION['sess_usersisname']); ?></a>
                     </div>
                     <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
-                        <a href="logout.php" class="btn btn-sm btn-secondary w-full w-lg-auto">Sair</a>
+                        <a href="logout.php" class="btn btn-sm btn-secondary w-full w-lg-auto"><i class="bi bi-box-arrow-right"></i> Sair</a>
                     </div>
                 </div>
             </div>
         </nav>
-        <br>
-        <div class="container">
-            <!--<h1>Bem-vindo, <b style="color: rgb(83 168 177)"><?php echo $_SESSION['sess_usersisname']; ?></b></h1>-->
+        <div class="container mt-4">
+            <?php if (isset($_SESSION['message'])) : ?>
+            <div class="alert alert-<?php echo $_SESSION['message']['type']; ?> alert-dismissible fade show" role="alert">
+                <?php echo $_SESSION['message']['text']; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php unset($_SESSION['message']); ?>
+            <?php endif; ?>
             <h1>Bem-vindo</h1>
             <p>Algumas funcionalidades podem não estar ativas ainda.</p>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
