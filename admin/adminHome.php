6928bb754c7cc85bddcc8433a87742b658d4c595
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-14 12:53:37 -0300

    Update adminHome.php

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -39,7 +39,7 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary px-0 py-3">
             <div class="container-xl">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="adminHome.php">
                     <img src="https://static.wixstatic.com/media/fef91e_c3f644e14da442178f706149ae38d838~mv2.png/v1/crop/x_0,y_24,w_436,h_262/fill/w_120,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/CAPA-03.png" class="h-12" alt="...">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
